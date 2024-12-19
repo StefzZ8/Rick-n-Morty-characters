2c7f5a3541cbf58d5c1d84d2bc30de8f3c536ca1
Stefko1998 <Simjanovski998@gmail.com>
2024-12-19 23:15:58 +0100

    setting up i18n

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import client from './apolloClient';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './utils/i18n.jsx';
+import client from './utils/apolloClient.jsx';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </ApolloProvider>
   </React.StrictMode>
-);  
+);
