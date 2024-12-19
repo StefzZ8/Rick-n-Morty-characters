fcc9dacd506c18986b24ceea8d2ae7c987fec170
Stefko1998 <Simjanovski998@gmail.com>
2024-12-19 12:31:53 +0100

    title for RnM deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.scss'
 import React from 'react';
 import CharacterList from './components/CharacterList';
 
 const App = () => {
   return (
     <div >
-      <h1>Rick and Morty Characters</h1>
       <CharacterList />
     </div>
   );
